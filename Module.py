1cce35a39b97224a8b658490f85453dd237040b7
vijay <17bmc037@mcet.in>
2024-01-28 18:04:31 -0500

    First commit

---
+++ Module.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def addition(x,y):
+    return x+y
+def subtraction(x,y):
+    return x-y
+def multiplication(x,y):
+    return x * y
+def division(x,y):
+    return x/y
+
+
+# In[ ]:
+
+
+
+
